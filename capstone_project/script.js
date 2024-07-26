ec90019633f047f5f7ab43aa94fc30263c5cf78c
AtalayASC24 <145040489+SuperSharkBoiii@users.noreply.github.com>
2024-07-26 09:17:28 -0700

    Add files via upload

---
+++ capstone_project/script.js
@@ -0,0 +1,32 @@
+// Get the button:
+let mybutton = document.getElementById("romanBtn");
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunctionR() {
+  document.body.scrollTop = 350; // For Safari
+  document.documentElement.scrollTop = 350; // For Chrome, Firefox, IE and Opera
+  doc
+m
+}
+
+let enriquebutton = document.getElementById("enriqueBtn");
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunctionE() {
+  document.body.scrollTop = 775; // For Safari
+  document.documentElement.scrollTop = 775; // For Chrome, Firefox, IE and Opera
+}
+
+let atalaybutton = document.getElementById("atalayBtn")
+
+function topFunctionA() {
+    document.body.scrollTop = 1200; // For Safari
+    document.documentElement.scrollTop = 1200; // For Chrome, Firefox, IE and Opera
+}
+  
+let adithyabutton = document.getElementById("adithyaBtn")
+
+function topFunctionD() {
+    document.body.scrollTop = 1500; // For Safari
+    document.documentElement.scrollTop = 1500; // For Chrome, Firefox, IE and Opera
+}
